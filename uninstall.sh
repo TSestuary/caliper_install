0727ebed9dede31376e26d68328db3d84b5ff93a
xingkai0713 <xingkai0713@thundersoft.com>
2018-01-08 11:37:12 +0800

    modify install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,147 +1,91 @@
 #!/bin/bash
 
-## show messages in a message box 
-# Usage: 4 parameters
-#	show_message [0:exit;1:success] [messages] [height] [width]
-show_message()
-{
-    whiptail --title "uninstall" --msgbox "$2" $3 $4
-    if [ $1 -eq 0 ]
-    then 
-        exit 0
-    fi
-}
-
-## write messages to the log file
-# Usage: 2 parameters 
-# 	write_log [message] [the log file]
-write_log()
-{
-    echo $1 >> $2 2>&1    
-}
-
-## output the start time of the uninstallation to the log file
-# Usage:
-#	1.output the start time of the uninstallation to the log file 
+## write the start time of the uninstallation to the log file
 uninstall_date()
 {
-    echo "[uninstall_date]" >> $log 2>&1
-    echo date=$(date +%Y-%m-%d_%H:%M:%S) >> $log 2>&1 
+    echo "[uninstall_date]" >> $log 
+    echo date=$(date +%Y-%m-%d_%H:%M:%S) >> $log 
 }
 
 ## uninstall caliper 
-# Usage:
-#	1.move to setup.py directory:  cd /opt/Caliper/caliper/     
-#	2.run setup.py:  sudo pip setup.py uninstall
-#	3.rm bin: sudo rm -rf /usr/local/bin/caliper
 uninstall_caliper()
 {
-    tmp_log="/tmp/caliper_tmp.log"
-    setup_py="/opt/Caliper/caliper/setup.py"
-    cd /opt/Caliper/caliper/
-    if [ ! -d $setup_py ]
-    then
-#        sudo pip $setup_py uninstall > $tmp_log 2>&1
-        sudo pip $setup_py uninstall 
-        echo "uninstall_caliper=ok" >> $log 2>&1
-    fi
-    cd - >> $tmp_log 2>&1
-    sudo rm -rf /usr/local/bin/caliper
+    sudo pip uninstall caliper -y > /dev/null 2>&1
+
+    sudo rm -f /usr/local/bin/caliper
 }
 
 ## uninstall pip new packages
-# Usage:
-#	1.get pip dependent new packages 
-#       2.uninstall each pip dependent package
 uninstall_pip()
 {
     tmp_log="/tmp/caliper_tmp.log"
-    packs=`cat /opt/Caliper/install*.log | grep "pip new" | uniq | sed 's/=.*$//'`
+    packs=`cat $HOME/caliper_output/install*.log | grep "pip install success" | uniq | sed 's/\ .*$//'`
     for pack in $packs
     do
         for((num=1;num<=3;num++))
         do
-            sudo pip uninstall $pack -y
-#            sudo pip uninstall $pack -y > $tmp_log 2>&1
-            if [ $? -ne 0 ]
+            sudo LC_ALL=C pip uninstall $pack -y >/dev/null 2>&1
+            if [ $? -eq 0 ]
             then
-                if [ $num -eq 3 ]
-                then
-                    echo "$pack=not uninstall" >> $log 2>&1
-                fi
-                continue
-            else
-                echo "$pack=uninstall" >> $log 2>&1
+                echo "$pack pip uninstall success" >> $log
                 break
             fi
+	    if [ $num -eq 3 ]; then
+	        echo "$pack pip uninstall failed" >> $log
+	    fi
         done
     done
 }
 
 ## uninstall dpk new packages
-# Usage:
-#	1.get dpk dependent new packages 
-#       2.uninstall each dpk dependent package
-uninstall_dpk()
+uninstall_pkg()
 {
     tmp_log="/tmp/caliper_tmp.log"
+
+    system_os=`cat /etc/os-release | grep -owP 'ID=\K\S+' | sed 's/"//g'`
+
     if [ $system_os = "ubuntu" ]
     then
-        packs=`cat /opt/Caliper/install*.log | grep "apt new" | uniq | sed 's/=.*$//'`
+        packs=`cat $HOME/caliper_output/install*.log | grep "pkg install success" | uniq | sed 's/\ .*$//'`
         for pack in $packs
         do
             for((num=1;num<=3;num++))
             do
-                sudo apt autoremove $pack -y 
-#                sudo apt autoremove $pack -y > $tmp_log 2>&1
-                if [ $? -ne 0 ]
+                sudo apt autoremove $pack -y > $tmp_log 2>&1
+                if [ $? -eq 0 ]
                 then
-                    if [ $num -eq 3 ]
-                    then
-                        echo "$pack=not uninstall" >> $log 2>&1
-                    fi   
-                    continue
-                else
-                    echo "$pack=uninstall" >> $log 2>&1
-                    break
+                    echo "$pack pkg uninstall success" >> $log 2>&1
+		    break
                 fi
+	        if [ $num -eq 3 ]; then
+                    echo "$pack pkg uninstall failed" >> $log 2>&1
+	        fi
             done
         done
     else
-        packs=`cat /opt/Caliper/install*.log | grep "yum new" | uniq | sed 's/=.*$//'`
+        packs=`cat $HOME/caliper_output/install*.log | grep "pkg install success" | uniq | sed 's/\ .*$//'`
         for pack in $packs
         do
             for((num=1;num<=3;num++))
             do
                 sudo yum autoremove $pack -y > $tmp_log 2>&1
-                if [ $? -ne 0 ]
+                if [ $? -eq 0 ]
                 then
-                    if [ $num -eq 3 ]
-                    then
-                        echo "$pack=not uninstall" >> $log 2>&1
-                    fi
-                    continue
-                else
-                    echo "$pack=uninstall" >> $log 2>&1
+                    echo "$pack pkg uninstall success" >> $log
                     break
                 fi
+	        if [ $num -eq 3 ]; then
+                    echo "$pack pkg uninstall failed" >> $log 2>&1
+	        fi
             done
         done
     fi
 }
  
-# Usage:
-#  	1. checkout user
-#	2. create log file
-#	3. read the installation log file
-#	4. show uninstall date
-#	5. show uninstall caliper
-#	6. show uninstall pip
-#	7. show uninstall dpk 
-#       8. choose remove ~/caliper_output and /opt/Caliper
+## main
+main()
 {
-
-## 1
+## 1. check user
 
 #    user=`whoami`
 #    if [ "$user" = "root" ]
@@ -149,64 +93,71 @@ uninstall_dpk()
 #        show_message 0 "   Please run this program as normal user." 7 48
 #    fi
 
-## 2
+## 1. check if caliper is installed
+    which caliper
+    if [ $? -ne 0 ]; then
+        whiptail --title "Error" --msgbox "             Could not find caliper." 7 55
+#	exit 1
+    fi
 
-    system_os=`cat /etc/os-release | grep -owP 'ID=\K\S+' | sed 's/"//g'`
-    log="/tmp/caliper_uninstall_$(date +%Y-%m-%d_%H-%M-%S).log"
-    echo $log
+## 2. create log file
+
+    log="$HOME/caliper_output/uninstall_$(date +%Y-%m-%d_%H-%M-%S).log"
     if [ ! -f $log ]
     then 
         touch $log
     fi
     sudo chown $user:$user $log
     
-## 3 
+## 3. read the installation log file
 
-    flag=`cat /opt/Caliper/install*.log | grep -c "caliper=ok"`
-    if [ $flag -eq 0 ]
-    then
-        show_message 0 "    checking install log fail then exit ..." 7 50 
-    fi 
+#    cat /opt/Caliper/install*.log | grep "caliper install success"
+#    if [ $? -ne 0 ]
+#    then
+#        show_message 0 "    checking install log fail then exit ..." 7 50 
+#    fi 
  
-## 4
+## 4. write uninstall date
 
-    uninstall_date
+    {
+    	uninstall_date
+    } | whiptail --title "Uninstall" --gauge "prepare log file" 7 55 10
 
-## 5
-    echo "[uninstall_caliper]" >> $log 2>&1
-    uninstall_caliper    
+## 5. uninstall caliper
 
-## 6
+    echo "[uninstall_caliper]" >> $log
+    {
+    	uninstall_caliper
+	echo " caliper uninstall success" >> $log
+    } | whiptail --title "Uninstall" --gauge "Uninstall caliper" 7 55 20
+
+## 6. uninstall pip packages
     
-    echo "[uninstall_pip]" >> $log 2>&1
-    uninstall_pip 
-    flag=`grep -c "not" $log`
-    if [ $flag -ne 0 ]
-    then
-        show_message 0 "checking uninstall pip fail then exit ..." 7 50 
-    fi
+    echo "[uninstall_pip]" >> $log
+    {
+    	uninstall_pip 
+    } | whiptail --title "Uninstall" --gauge "Uninstall pip packages" 7 55 40
 
-## 7
+## 7. uninstall dpkg/rpm pacakges
 
-    echo "[uninstall_dpk]" >> $log 2>&1
-    uninstall_dpk
-    flag=`grep -c "not" $log`
-    if [ $flag -ne 0 ]
-    then
-        show_message 0 "checking uninstall dpk fail then exit ..." 7 50 
-    fi
+    echo "[uninstall_pkg]" >> $log
+    {
+    	uninstall_pkg
+    } | whiptail --title "Uninstall" --gauge "Uninstall dpkg/rpm packages" 7 55 70
 
-## 8
+## 8. remove output and source
 
-    whiptail --yesno "Please choose remove ~/caliper_output and /opt/Caliper" 7 58
+    whiptail --yesno "Please choose remove ~/caliper_output and /opt/Caliper" --defaultno 7 58
     choose_status=$?
     if [ $choose_status -eq 0 ]
     then
         rm -rf ~/caliper_output
         sudo rm -rf /opt/Caliper
     fi
-    show_message 1 "uninstall caliper success then exit ..." 7 50  
-    exit 0
+
+## 9. finish
+   whiptail --title "uninstall caliper" --msgbox "             uninstall caliper finished." 7 58 
+   exit 0
 }
 
 main
